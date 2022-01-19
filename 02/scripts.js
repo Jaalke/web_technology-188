caf4e6f8b90ba5382a14ef1b644207375d3841c2
Jaalke <adamski.jan.franciszek@gmail.com>
2022-01-19 14:27:58 +0100

    Created a basic sorting function in JQuery using a bubble sort algorithm. Applied to the second table.

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,14 +1,36 @@
+// helper function for swapping rows in a table, used to sort
+
 function swapRows(rowOne, rowTwo) {
-    rowOne = rowOne.replaceWith(rowTwo);
+    rowOne.replaceWith(rowTwo);
     rowTwo.after(rowOne);
 }
 
-function sortTable(id) {
+// sorting function using the bubble sort algorithm
 
+function sortTable(table, columnIndex) {
+    let rows = table.children("tbody").children();
+    let isSorted = false;
+
+    while (!isSorted) {
+        isSorted = true;
+
+        for (let i=0; i<rows.length - 1; i++) {
+            rows = table.children("tbody").children();
+            let firstRow = rows.eq(i);
+            let secondRow = rows.eq(i + 1);
+            let firstVal = firstRow.children().eq(columnIndex).text();
+            let secondVal = secondRow.children().eq(columnIndex).text();
+
+            if (firstVal.localeCompare(secondVal, undefined, { numeric: true }) == 1) {
+                swapRows(firstRow, secondRow);
+                isSorted = false;
+            } 
+        }
+    }
 }
 
 $(".header").click(
     function(){
-        swapRows($("#testOne"), $("#testTwo"));
+        sortTable($(this).parents("table"), $(this).index());
     }
 );
