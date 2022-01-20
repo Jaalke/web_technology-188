8b445f26e631fe9a591d43dbc7ff84633d28f4c3
Jaalke <adamski.jan.franciszek@gmail.com>
2022-01-20 23:44:48 +0100

    Fully dynamic product table using AJAX. Input row now stays at the bottom of the table after sorting.

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,15 +1,36 @@
-// helper function for swapping rows in a table, used to sort
+// helper function for swapping rows in a table, used to sort:
 
 function swapRows(rowOne, rowTwo) {
     rowOne.replaceWith(rowTwo);
     rowTwo.after(rowOne);
+    return;
 }
 
-// sorting function using the bubble sort algorithm
+// helper function for restoring the input row to the bottom of the table:
+// not exactly elegant but does the job ¯\_(ツ)_/¯
 
-function sortTable(table, columnIndex) {
+function inputRowToBottom (table) {
+    let body = table.children("tbody");
+    body.after(body.children(".exceptRow"));
+}
+
+// sorting function using the bubble sort algorithm:
+
+function sortTable(table, columnIndex, reversed) {
     let rows = table.children("tbody").children();
     let isSorted = false;
+    let numberOfChanges = 0;
+    let d = 1;
+    let inputRow = NaN;
+
+    // if (rows.hasClass("exceptRow")) {
+    //     alert("reposition triggered");
+    //     inputRow = rows.filter(".exceptRow").detach();
+    // }
+
+    if (reversed) {
+        d = -1;
+    }
 
     while (!isSorted) {
         isSorted = true;
@@ -21,16 +42,61 @@ function sortTable(table, columnIndex) {
             let firstVal = firstRow.children().eq(columnIndex).text();
             let secondVal = secondRow.children().eq(columnIndex).text();
 
-            if (firstVal.localeCompare(secondVal, undefined, { numeric: true }) == 1) {
+            if (firstVal.localeCompare(secondVal, undefined, { numeric: true }) == d) {
                 swapRows(firstRow, secondRow);
                 isSorted = false;
-            } 
+                numberOfChanges++;
+            }
         }
     }
+
+    // the code below automatically sorts in the opposite direction
+    // if the table is already sorted
+
+    if (numberOfChanges == 0) {
+        sortTable(table, columnIndex, !reversed);
+    }
+
+    return;
 }
 
-$(".header").click(
+// refresh function, inserts ALL data from the server into the product table:
+
+function refreshTable(table, data) {
+
+    for (i in data) {
+        let lastRow = table.children("tbody").children();
+        lastRow = lastRow.eq(lastRow.length - 2)
+
+        let insert = "";
+        insert += "<tr>"
+        insert += "<td>" + data[i].brand + "</td>";
+        insert += "<td>" + data[i].model + "</td>";
+        insert += "<td>" + data[i].os + "</td>";
+        insert += "<td>" + data[i].screensize + "</td>";
+        insert += "<td><a href=\"" + data[i].image + "\">link</a></td>";
+        insert += "</tr>";
+
+        lastRow.before(insert);
+    }
+
+    return;
+}
+
+// listeners:
+
+$("th").click(
     function(){
-        sortTable($(this).parents("table"), $(this).index());
+        sortTable($(this).parents("table"), $(this).index(), false);
+        inputRowToBottom($(this).parents("table"));
+    }
+);
+
+$("#productTable").ready( 
+    function(){
+        $.get("https://wt.ops.labs.vu.nl/api22/6fa3add2", function(data, status){
+            // alert("Data: " + data + "\nStatus: " + status);
+            refreshTable($("#productTable"), data);
+        });
     }
 );
